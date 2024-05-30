53d2003d3bcc7f68af1663f79ee2417ee4270cfb
knosarev <nosakonstantin@gmail.com>
2024-05-30 20:32:32 +0700

    Init commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../App.css";
+
+const Footer = () => {
+    return (
+        <div className="attribution">
+            Challenge by
+            <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+                rel="noopener"
+            >
+                Frontend Mentor
+            </a>
+            . Coded by
+            <a
+                href="https://github.com/marcinsuski"
+                target="_blank"
+                rel="noopener"
+            >
+                Marcin Suski
+            </a>
+            .
+        </div>
+    );
+};
+
+export default Footer;
